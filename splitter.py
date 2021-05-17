3513b4b46bcbcd7a9652624ff723c6ce603a7cf4
Polina <rasskazova.pm@phystech.edu>
2021-05-17 11:54:41 +0300

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+from Bio import SeqIO
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--sequence', dest='sequence', type=str, help='Path to file with sequence', required=True)
@@ -10,38 +12,22 @@ parser.add_argument('-i', '--interval', dest='interval', type=int,
 parser.add_argument('-o', '--output', dest='output', type=str, default='split_sequence', help='Path to output file')
 args = parser.parse_args()
 
-file = open(args.sequence)
-
-sequence_name = ''
-sequence = ''
-
-is_first_line = True
-for line in file:
-    if not is_first_line:
-        sequence += line.replace('\n', '')
-    else:
-        sequence_name = line
-        is_first_line = False
-
 
 if not os.path.exists(args.output):
     os.mkdir(args.output)
 
 output = open(args.output + '.fna', 'w')
 
+with open(args.sequence) as handle:
+    for record in SeqIO.parse(handle, "fasta"):
+        for i in range(0, len(record.seq), args.interval):
 
-def prepare_sequence_name(name, start_index, length):
-    insert_interval = '_' + str(start_index) + '-' + str(start_index + length)
-    index_of_space = name.find(' ')
-    result_name = name[:index_of_space] + insert_interval + name[index_of_space:]
-    return result_name
+            id_with_interval = record.id + '_' + str(i + 1) + '-' + str(i + args.length + 1)
+            description = '>' + record.description.replace(record.id, id_with_interval) + '\n'
 
+            if i + args.length < len(record.seq):
+                output.write(description + str(record.seq[i:i + args.length]) + '\n')
 
-for i in range(0, len(sequence), args.interval):
-    prepared_name = prepare_sequence_name(sequence_name, i + 1, args.length - 1)
-
-    if i + args.length < len(sequence):
-        output.write(prepared_name + sequence[i:i + args.length] + '\n')
-    else:
-        output.write(prepared_name + sequence[-args.length:])
-        break
+            else:
+                output.write(description + str(record.seq[-args.length:]))
+                break
